855b7d3a60fca5ffc3562d7a35e5ac2d19277044
DK00715 <antonio.costa.guilherme@escola.pr.gov.br>
2025-10-03 14:17:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const avanca = document.queryselectorA('.btn-proximo')
 
+avanca.forEach(button => {
+  button.addEventListener('click', function(){
+const atual = document.querySelector('.ativo')
+const proximoPasso = 'passo-' + this.getAttribute('data-proximo')  
+
+ atual.classList.remove('ativo');
+ document.getElementById9proximoPasso).classList.add('ativo');   
+  })
+})                        
